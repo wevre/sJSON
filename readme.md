47bbabceed6ba8c621733b86156b52e84ffccf4f
wevrem <wevrem@users.noreply.github.com>
2016-11-04 11:33:06 -0600

    initial commit

---
+++ readme.md
@@ -0,0 +1,394 @@
+#sJSON
+
+##Availability
+
+This repository has code to implement `sJSON` in Python, PHP, and Swift. You can use this code to run `sJSON` as a command-line converter or to create a module to integrate into your code base.
+
+##Introduction
+
+Take this valid `JSON` string:
+
+```json
+[{“books”:[“Cat in the Hat”,”Are You My Mother?”,”Fox in Socks”]},{“colors”:[“red”,”yellow”,
+”blue”]},{“flavors”:[“chocolate”,”vanilla”,”strawberry”]}]
+```
+
+A computer has no problem reading that, but a human might struggle. When humans are involved, we spread things out and line things up to make it easier to see what’s going on.
+
+```json
+[
+     {
+          “books”: [
+               “Cat in the Hat”,
+               “Are You My Mother?”,
+               “Fox in Socks”
+          ]
+     },
+     {
+          “colors”: [
+               “red”,
+               ”yellow”,
+               ”blue”
+          ]
+     },
+     {
+          “flavors”: [
+               “chocolate”,
+               ”vanilla”,
+               ”strawberry”
+          ]
+     }
+]
+```
+
+This extra spacing is helpful for the humans, but the computer ignores it. It still needs braces and brackets, commas and colons, and quotation marks to interpret it all correctly.
+
+Let’s make it easier on us humans. Since we naturally rely on spacing, let’s make that meaningful, and the computer can figure out the special punctuation. That’s exactly what `sJSON` let’s you do. Here is our data written out using `sJSON`.
+
+```txt
+books:
+	Cat in the Hat
+	Are You My Mother?
+	Fox in Socks
+
+colors:
+	red
+	yellow
+	blue
+
+flavors:
+	chocolate
+	vanilla
+	strawberry
+```
+
+`sJSON` is easier for humans to both read and write, and we’ll let the computer do the work of figuring out where punctuation needs to go to render this as valid JSON. There are only a few rules to follow.
+
+##Basic values
+
+To create a `JSON` array, write each element on its own line using the same indentation.
+
+<table>
+	<tr><td>
+```sjson
+Tom
+Dick
+Harry
+```
+</td>
+<td>
+```json
+[
+     “Tom”,
+     “Dick”,
+     “Harry”
+]
+```
+</td></tr></table>
+
+To create a JSON object, separate the key and value with a colon. Write each key-value pair on its own line using the same indentation.
+
+City: Tulsa
+Population: 45 million
+Location: 45’ W 37’N
+{
+     “City”: “Tulsa”,
+     “Population” : “45 million”,
+     “Location” : “45’ W 37’N”
+}
+
+
+String values, as you can see above, are written without quotations marks. Usually. More on that later.
+Nested values
+To nest arrays, write a hyphen all by itself as an array marker, then indent the next lines.
+
+-
+     1
+     2
+     3
+
+-
+     red
+     yellow
+     blue
+[
+     [
+          1,
+          2,
+          3
+     ],
+     [
+          “red”,
+          “yellow”,
+          “blue”
+     ]
+]
+
+
+To nest inside an object, write the key and colon on one line, then indent the next lines.
+
+flavors:
+     chocolate
+     vanilla
+     strawberry
+{
+     “flavors”: [
+          “chocolate”,
+          “vanilla”,
+          “strawberry”
+     ]
+}
+
+
+
+Cheating with commas
+You can use commas to separate simple array elements on the same line.
+
+numbers :
+     1,2,3
+
+colors :
+     red, yellow, blue
+{
+     “numbers”: [
+          1,
+          2,
+          3
+     ],
+     “colors”: [
+          “red”,
+          “yellow”,
+          “blue”
+     ]
+}
+
+
+You can do the same comma trick with objects that contain simple values only.
+Name: Tom, Age: 42, City: Miami
+
+Name: Fred, Age: 37, City: Houston
+
+Name: Lucy, Age: 43, City: Cincinnati
+
+[
+     {
+          “Name”: “Tom”,
+          “Age”: 42,
+          “City”: “Miami”
+     },
+     {
+          “Name”: “Fred”,
+          “Age”: 37,
+          “City”: “Houston”
+     },
+     {
+          “Name”: “Lucy”,
+          “Age”: 43,
+          “City”: “Cincinnati”
+     }
+]
+
+
+
+String values
+Notice we have not used quotes at all so far. sJSON is smart about putting quotes around your strings. Here are some things to keep in mind:
+•     Colons and commas are markers for arrays and objects. If you don’t want those interpreted as markers, they need to be inside a quoted string.
+•     A hyphen on a line by itself is an array marker. If you want it to be a string instead, then put quotes around it.
+•     sJSON will recognize and not put quotes around JSON numbers and JSON literals (true, false, null).
+•     Except for object keys. Only strings are allowed as object keys, so whatever you write for the key will always get quoted.
+•     We’ll add to this list after we’ve covered comments and raw JSON below.
+[[NEED SOME EXAMPLES]]
+
+Empty values
+JSON allows empty arrays and empty objects. To write those in simpJSON, use a lone comma or a lone colon.
+
+list of nobel prizes I’ve won so far:
+     ,
+{ “list of nobel prizes won I’ve won so far”: [] }
+
+
+
+
+## a whole lot of nothing
+:
+:
+:
+[
+     {},
+     {},
+     {},
+]
+
+
+Comments
+As you might have noticed in the last example, two pound signs will mark the rest of the line as a comment.
+
+members:
+     Larry
+     Dana
+     ##Fred – he dropped out last month
+     Tom
+{
+     “members”: [
+          “Larry”,
+          “Dana”,
+          “Tom”
+     ]
+}
+
+
+So in addition to commas and colons, if you need two pound signs to be output as part of a string, the string needs to be quoted.
+
+Records
+Look at this example again. These table-like structures, with the same keys repeated for each object, come up all the time.
+
+Name: Tom, Age: 42, City: Miami
+Name: Fred, Age: 37, City: Houston
+Name: Lucy, Age: 43, City: Cincinnati
+
+[
+     {
+          “Name”: “Tom”,
+          “Age”: 42,
+          “City”: “Miami”
+     },
+     {
+          “Name”: “Fred”,
+          “Age”: 37,
+          “City”: “Houston”
+     },
+     {
+          “Name”: “Lucy”,
+          “Age”: 43,
+          “City”: “Cincinnati”
+     }
+]
+
+
+sJSON has a shortcut for you, called a record, which you trigger with a double colon.
+
+:: Name, Age, City
+     Tom, 42, Miami
+     Fred, 37, Houston
+     Lucy, 43, Cincinnati
+
+[
+     {
+          “Name”: “Tom”,
+          “Age”: 42,
+          “City”: “Miami”
+     },
+     {
+          “Name”: “Fred”,
+          “Age”: 37,
+          “City”: “Houston”
+     },
+     {
+          “Name”: “Lucy”,
+          “Age”: 43,
+          “City”: “Cincinnati”
+     }
+]
+
+
+Isn’t that awesome! You specify the keys one time, after the double colon, and then write just the values on the lines that follow. Indented, of course.
+
+If you have a record nested inside an object, you can write it like this.
+
+## You can write
+
+people:
+     :: Name, Age, City
+          Tom, 42, Miami
+          Fred, 37, Houston
+          Lucy, 43, Cincinnati
+
+
+## Or you can collapse
+
+people :: Name, Age, City
+     Tom, 42, Miami
+     Fred, 37, Houston
+     Lucy, 43, Cincinnati
+
+
+{
+     “people”: [
+          {
+               “Name”: “Tom”,
+               “Age”: 42,
+               “City”: “Miami”
+          },
+          {
+               “Name”: “Fred”,
+               “Age”: 37,
+               “City”: “Houston”
+          },
+          {
+               “Name”: “Lucy”,
+               “Age”: 43,
+               “City”: “Cincinnati”
+          }
+     ]
+}
+
+
+Keys and values should be in the same order. You can leave a value blank and it will be skipped in the output JSON.
+[[put an example here of leaving a value blank]]
+
+Warnings and errors
+In sJSON, indentation is allowed (and expected) after an array marker, for nesting objects, and for record sets. If you write one of those but don’t indent the next lines, then instead of being interpreted as array or object markers, they will revert to strings and sJSON will emit a warning.
+
+-
+oops
+not
+nested
+[
+     “-“,
+     “oops”,
+     “not”,
+     “nested”
+]
+
+Warning: Expecting indentation after array marker “-“ on line 1.
+
+
+If you indent in an unexpected place, sJSON will emit an error and stop.
+
+Can, I, do, this:
+     the
+     answer
+     is
+     no
+Warning: Object is missing a value on line 1. Converted to string.
+Can, I, do, this:
+            ^^^^^
+Error: Illegal indentation on line 2.
+
+
+
+
+Can
+I
+do
+this:
+     the
+     answer
+     is
+     yes
+[
+     “Can”,
+     “I”,
+     “do”,
+     {
+          “this”: [
+               “the”,
+               “answer”,
+               “is”,
+               “yes”
+          ]
+     }
+]
+
+
+
---
+++ sjson.py
@@ -0,0 +1,393 @@
+#!/usr/bin/python
+#
+#  simpjson.py
+#
+#  Created by Mike Weaver on 2016-10-27.
+#
+
+#TODO: deal with quotes, numbers, literals (i.e., when to add quotes and when not to)
+#TODO: deal with arrays of pairs (so they wrap with braces instead of brackets)
+#TODO: remember to .strip() the underlying Name tokens
+#TODO: JSON mode needs to support multiline
+
+import sys
+import re
+
+#
+# Tokens
+#
+
+class Token:
+
+	def __init__(self, text, line, pos):
+		self.text = text
+		self.lineNumber = line
+		self.charPosition = pos
+
+	def __str__(self):
+		return '{{{0}:`{1}`,{2}:{3}}}'.format(self.__class__.__name__[:-5].upper(), self.text, self.lineNumber, self.charPosition)
+
+	def pushChar(self, chr):
+		self.text += chr
+
+	def popChar(self):
+		self.text = self.text[:-1]
+
+class IndentToken(Token):
+	pass
+
+class NameToken(Token):
+	pass
+
+class CommaToken(Token):
+	pass
+
+class ColonToken(Token):
+	pass
+
+class DoubleColonToken(Token):
+	pass
+
+class CommentToken(Token):
+	pass
+
+class JSONToken(Token):
+	pass
+
+#
+# Lexer
+#
+
+class Lexer:
+
+	def __init__(self, file):
+		self.file = file
+		self.lineNumber = 0
+		self.charPosition = 0
+
+	#
+	# Managing tokens
+	#
+
+	def currentToken(self):
+		return self.tokenList[-1] if self.tokenList else None
+
+	def twoConsecutiveColons(self, token):
+		latest = self.currentToken()
+		return (latest and isinstance(latest, ColonToken) and ':' == latest.text and isinstance(token, ColonToken))
+
+	def addToken(self, token):
+		if self.twoConsecutiveColons(token):
+			original = self.tokenList.pop()
+			token = DoubleColonToken('::', original.lineNumber, original.charPosition)
+		self.tokenList.append(token)
+
+	def evalCurrItem(self):
+		'''This function is not used right now, but the logic will be useful when we are ready to generate the output.'''
+		item = self.currItem.strip()
+		if not item: return
+		if item == 'null' or item == 'false' or item == 'true':
+			token = NameToken(item, self.lineNumber, self.charPosition)
+		elif re.match('^-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?$', item):
+			token = NameToken(item, self.lineNumber, self.charPosition)
+		else:
+			hasQuotes = item.endswith('"') and item.startswith('"')
+			token = NameToken(item, self.lineNumber, self.charPosition)
+		self.tokenList.append(token)
+
+	#
+	# Scanning characters and scanner callbacks
+	#
+
+	def scan_POUND(self, chr):
+		token = self.currentToken()
+		if token and isinstance(token, NameToken) and token.text.endswith('#'):
+			token.popChar()
+			if not token.text:
+				self.tokenList.pop()
+			dptoken = CommentToken('##', self.lineNumber, self.charPosition-1)
+			self.addToken(dptoken)
+		else:
+			self.scan_CHARACTER('#')
+
+	def scan_CHARACTER(self, chr):
+		token = self.currentToken()
+		if not token or not isinstance(token, NameToken):
+			self.addToken(NameToken(chr, self.lineNumber, self.charPosition))
+		elif isinstance(token, NameToken):
+			token.pushChar(chr)
+
+	def scanLine(self, line):
+		self.tokenList = []
+		self.charPosition = 0
+		stringMode = False
+		commentMode = False
+		prev = ''
+		jsonStack = []
+		jsonMode = False
+		for chr in line:
+			# EOL character
+			if '\n' == chr:
+				pass
+			# Comment mode
+			elif commentMode:
+				token = self.currentToken()
+				assert token and isinstance(token, CommentToken)
+				token.pushChar(chr)
+			# JSON mode
+			elif jsonMode:
+				token = self.currentToken()
+				assert token and isinstance(token, JSONToken)
+				token.pushChar(chr)
+				if '[' == chr or '{' == chr:
+					jsonStack.push(chr)
+				elif (']' == chr and '[' == jsonStack[-1]) or ('}' == chr and '{' == jsonStack[-1]):
+					jsonStack.pop()
+				elif (']' == chr and '{' == jsonStack[-1]) or ('}' == chr and '[' == jsonStack[-1]):
+					print 'error: mismatched JSON delimiters'
+					# we should report an error
+					pass
+				if not jsonStack:
+					jsonMode = False
+			# Whitespace
+			elif '\t' == chr or ' ' == chr:
+				token = self.currentToken()
+				if not token:
+					self.addToken(IndentToken(chr, self.lineNumber, self.charPosition))
+				else:
+					token.pushChar(chr)
+			# Colon
+			elif ':' == chr and not stringMode:
+				self.addToken(ColonToken(chr, self.lineNumber, self.charPosition))
+			# Comma
+			elif ',' == chr and not stringMode:
+				self.addToken(CommaToken(chr, self.lineNumber, self.charPosition))
+			# Pound
+			elif '#' == chr and not stringMode:
+				self.scan_POUND(chr)
+				if '#' == prev:
+					commentMode = True
+			# JSON
+			elif ('[' == chr or '{' == chr) and not stringMode:
+				self.addToken(JSONToken(chr, self.lineNumber, self.charPosition))
+				jsonMode = True
+			# Beginning quote
+			elif '"' == chr and not stringMode:
+				self.scan_CHARACTER(chr)
+				stringMode =True
+			# Ending quote
+			elif '"' == chr and stringMode:
+				if '\\' != prev:
+					stringMode = False
+				self.scan_CHARACTER(chr)
+			# Any character
+			else:
+				self.scan_CHARACTER(chr)
+			prev = chr
+			self.charPosition += 1
+		return self.tokenList
+
+	def getTokens(self):
+		for line in self.file:
+			#TODO: If we want, we could skip over empty lines, and INDENT/COMMENT-only lines right here
+			yield self.scanLine(line)
+			self.lineNumber += 1
+
+#
+# Node classes
+#
+
+class Node:
+
+	indent = ''
+
+	def __init__(self, indent, payload = None, children = None, keys = None):
+		self.indent = indent #NOTE: This will be an IndentToken or None (for the outermost array)
+		self.children = children if children else [] #NOTE: These will be Nodes.
+		self.payload = payload #NOTE: This will be a Token or None.
+		self.keys = keys #This will be an array of Strings
+
+	def __str__(self):
+		result = Node.indent + self.__class__.__name__[:-4].upper() + ':'
+		if self.payload:
+			result += ' payload=`' + self.payload.text.strip() + '`'
+		if self.keys:
+			result += ' keys=[' + ', '.join(c.payload.text.strip() for c in self.keys.children) + ']'
+		if self.children:
+			result += ' children=[\n'
+			Node.indent += ' '*5
+			result += '\n'.join(str(c) for c in self.children)
+			Node.indent = Node.indent[:-5]
+			result += '\n' + Node.indent + ']'
+		return result
+
+class StringNode(Node):
+	pass
+
+class PairNode(Node):
+	pass
+
+class ArrayNode(Node):
+	pass
+
+class RecordNode(Node):
+	pass
+
+#
+# Parser
+#
+
+class Parser:
+
+	def __init__(self, lexer):
+		self.lexer = lexer
+
+	def parseArrayMarker(self, tokens):
+		target = 1 if isinstance(tokens[0], IndentToken) else 0
+		if 1+target == len(tokens) and isinstance(tokens[target], NameToken) and '-' == tokens[target].text.strip():
+			self.parsedNode = ArrayNode(self.indent, tokens[target])
+			return True
+		return False
+
+	def parseOpenPair(self, tokens):
+		target = 1 if isinstance(tokens[0], IndentToken) else 0
+		if 2+target == len(tokens) and isinstance(tokens[target], NameToken) and isinstance(tokens[target+1], ColonToken):
+			self.parsedNode = PairNode(self.indent, tokens[target])
+			return True
+		return False
+
+	def parseRecordDef(self, tokens):
+		target = 1 if isinstance(tokens[0], IndentToken) else 0
+		name = None
+		if 1+target < len(tokens) and isinstance(tokens[target], NameToken):
+			name = tokens[target]
+			target += 1
+		if not (1+target < len(tokens)) or not isinstance(tokens[target], DoubleColonToken):
+			return False
+		if not self.parseValueList(tokens[target+1:], True): #NOTE: We turn stringOnly mode on for this call to parseValueList().
+			return False
+		keys = self.parsedNode
+		#TODO: check for errors, if the parsedNode is not at array, turn it into one. If it is empty, we have an error
+		self.parsedNode = RecordNode(self.indent, name, None, keys)
+		return True
+
+	def parseFieldList(self, tokens):
+		pass
+
+	def parseString(self, tokens):
+		assert tokens
+		target = 1 if isinstance(tokens[0], IndentToken) else 0
+		self.parsedNode = StringNode(self.indent, self.mergeTokens(tokens[target:]))
+		return True
+
+	def parseValue(self, tokens):
+		'''Parses a single value, the stuff you would find between commas.'''
+		assert tokens
+		target = 1 if isinstance(tokens[0], IndentToken) else 0
+		if 1+target == len(tokens) and isinstance(tokens[target], CommaToken):
+			self.parsedNode = ArrayNode(self.indent)
+			return True
+		if 1+target == len(tokens) and isinstance(tokens[target], ColonToken):
+			self.parsedNode = PairNode(self.indent)
+			return True
+		if 1+target == len(tokens) and isinstance(tokens[target], JSONToken):
+			self.parsedNode = JSONNode(self.indent, tokens[target])
+			return True
+		if 2+target < len(tokens) and isinstance(tokens[target], NameToken) and isinstance(tokens[target+1], ColonToken):
+			self.parsedNode = PairNode(self.indent, tokens[target], [ StringNode(self.indent, self.mergeTokens(tokens[target+2:])) ])
+			return True
+		#TODO: let's test for explicit {COLON}{NAME} or {NAME}{COLON} and create a partial pair that will fail when it gets to JSON
+		if any(isinstance(t, ColonToken) for t in tokens):
+			# here we put out a warning that the user has a partial pair that will be treated as a string
+			pass
+		self.parsedNode = StringNode(self.indent, self.mergeTokens(tokens[target:]))
+		return True
+
+	def parseValueList(self, tokens, stringOnly = False):
+		'''Parses a comma-separated list of tokens.'''
+		ndlist = []
+		commaFlag = False
+		target = 1 if isinstance(tokens[0], IndentToken) else 0
+		while target < len(tokens):
+			tklist = []
+			while target < len(tokens) and isinstance(tokens[target], CommaToken):
+				commaFlag = True
+				target += 1
+			while target < len(tokens) and not isinstance(tokens[target], CommaToken):
+				tklist.append(tokens[target])
+				target += 1
+			if tklist and ((stringOnly and self.parseString(tklist)) or self.parseValue(tklist)):
+				ndlist.append(self.parsedNode)
+		if 1 == len(ndlist) and not commaFlag:
+			self.parsedNode = ndlist[0]
+		else:
+			self.parsedNode = ArrayNode(self.indent, None, ndlist)
+		return True
+
+	def mergeTokens(self, tokens):
+		'''Combines the text members from each token in the list into a single Name token.'''
+		assert tokens
+		newText = reduce(lambda a, b: a + b, map(lambda a: a.text, tokens), '')
+		return NameToken(newText, tokens[0].lineNumber, tokens[0].charPosition)
+
+	def getNodes(self):
+		'''Steps through all the tokens returned from the lexer and constructs a hierarchy of Node objects.'''
+		indent_none = IndentToken('', 0, 0)
+		nodeStack = [ ArrayNode(None) ]
+		lineNumber = 0
+		for tokenList in self.lexer.getTokens():
+			print 'line ' + str(lineNumber) + ': ' + ', '.join(str(t) for t in tokenList) + '\n'
+			# Filter the tokens to remove blank lines, comment-only lines, etc.
+			if all(isinstance(t, IndentToken) or isinstance(t, CommentToken) for t in tokenList): # NOTE: This also handily returns true for an empty token list.
+				continue
+			tokenList = filter(lambda t: not isinstance(t, CommentToken), tokenList)
+			self.indent = tokenList[0] if isinstance(tokenList[0], IndentToken) else indent_none
+			# Find the right place for new nodes, popping nodes off the stack if needed.
+			while nodeStack:
+				cursor = nodeStack[-1]
+				if not cursor.indent or (self.indent.text > cursor.indent.text and self.indent.text.startswith(cursor.indent.text)):
+					if not cursor.children or cursor.children[-1].indent.text == self.indent.text:
+						#everything is okay
+						pass
+					else:
+						# we have an error, because the indent is greater than the parent, but doesn't match prior siblings
+						print "error because indent is greater than parent, but doesn't match prior siblings"
+						return None
+					break
+				else:
+					finished = nodeStack.pop()
+					#TODO: this might be a place to examine the just-popped container and emit warnings if stuff is empty that we expected to have children.
+			if not nodeStack:
+				print 'error: we popped our way off the node stack'
+				return None
+			# Generate the new node from the current line of tokens
+			cursor = nodeStack[-1]
+			if cursor.keys:
+				if self.parseValueList(tokenList):
+					newNode = self.parsedNode
+				else:
+					print 'error parsing field list in table mode'
+					return None
+			elif self.parseArrayMarker(tokenList) or self.parseOpenPair(tokenList) or self.parseRecordDef(tokenList):
+				newNode = self.parsedNode
+				nodeStack.append(newNode)
+			elif self.parseValueList(tokenList):
+				newNode = self.parsedNode
+			else:
+				print 'error: unable to parse anything on this line: ' + lineNumber
+				return None
+			cursor.children.append(newNode)
+			lineNumber += 1
+		# here we are done with the tokens.
+		return nodeStack[0]
+
+#
+# Interactive mode
+#
+
+if __name__ == '__main__':
+	lexer = Lexer(sys.stdin)
+	parser = Parser(lexer)
+	# write string representation of parser's getNodes() to stdout
+	sys.stdout.write(str(parser.getNodes()))
+	print
